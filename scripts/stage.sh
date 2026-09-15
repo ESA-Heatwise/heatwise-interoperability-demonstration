b95c3b4efd5eed81155c3c9c73d8e045a1371bd3
Hannes Neuschmidt <hannes.neuschmidt@brockmann-consult.de>
2026-09-15 18:45:57 +0200

    Add staging for LCZ products

--- scripts/stage.sh
+++ scripts/stage.sh
@@ -11,24 +11,64 @@ mkdir -p $INPUTS_PATH
 mkdir -p $CWL_PATH
 
 stage_lstm_wp3_products() {
-    git clone --branch "$GIT_REF" git@github.com:ESA-Heatwise/lstm-wp3-products.git $SOFTWARE_PATH/lstm-wp3-products
-    products_base=$SOFTWARE_PATH/lstm-wp3-products/products
+    REPO_BASE="$SOFTWARE_PATH/lstm-wp3-products"
+    git clone --branch "$GIT_REF" git@github.com:ESA-Heatwise/lstm-wp3-products.git "$REPO_BASE"
+    PRODUCTS_BASE="$REPO_BASE/products"
 
     # hotspots
-    cp "$products_base/hotspots/app/eoap.cwl" "$CWL_PATH/heatwise-hotspots.cwl"
+    cp "$PRODUCTS_BASE/hotspots/app/eoap.cwl" "$CWL_PATH/heatwise-hotspots.cwl"
     # inertia
-    cp "$products_base/inertia/app/eoap.cwl" "$CWL_PATH/heatwise-inertia.cwl"
+    cp "$PRODUCTS_BASE/inertia/app/eoap.cwl" "$CWL_PATH/heatwise-inertia.cwl"
 
     # combined urban atlas hotspots materials
-    cp "$products_base/ua/app/eoap.cwl" "$CWL_PATH/heatwise-combined-hotspots-materials.cwl"
+    cp "$PRODUCTS_BASE/ua/app/eoap.cwl" "$CWL_PATH/heatwise-combined-hotspots-materials.cwl"
 
 
     mkdir -p $INPUTS_PATH/{hotspots,inertia,combined_hotspots_materials}
-    cp -r "$products_base"/hotspots/inputs/* $INPUTS_PATH/hotspots
+    cp -r "$PRODUCTS_BASE"/hotspots/inputs/* $INPUTS_PATH/hotspots
 
-    cp -r "$products_base"/inertia/inputs/* $INPUTS_PATH/inertia
+    cp -r "$PRODUCTS_BASE"/inertia/inputs/* $INPUTS_PATH/inertia
 
-    cp -r "$products_base"/ua/inputs/* $INPUTS_PATH/combined_hotspots_materials
+    cp -r "$PRODUCTS_BASE"/ua/inputs/* $INPUTS_PATH/combined_hotspots_materials
+}
+
+stage_hysupp() {
+    REPO_BASE=$SOFTWARE_PATH/heatwise-hysupp-unmixing
+
+    git clone --branch "$GIT_REF" git@github.com:ESA-Heatwise/heatwise-hysupp-unmixing.git "$REPO_BASE"
+
+    # cwl
+    cp "$REPO_BASE/heatwise_hysupp_unmixing.cwl" "$CWL_PATH/"
+
+
+    # inputs
+    mkdir -p $INPUTS_PATH/unmixing
+    cp -r "$REPO_BASE/data" "$INPUTS_PATH/unmixing/"
+}
+
+stage_lcz() {
+    PREP_ROOT="$SOFTWARE_PATH/heatwise-hsi-lst-prep"
+    PATCH_ROOT="$SOFTWARE_PATH/heatwise-patch-extraction"
+    CLASSIFICATION_ROOT="$SOFTWARE_PATH/heatwise-lcz-classificaton"
+    PIPELINE_ROOT="$SOFTWARE_PATH/pipeline"
+
+    git clone --branch "$GIT_REF" git@github.com:ESA-Heatwise/heatwise-hsi-lst-prep.git $PREP_ROOT
+    git clone --branch "$GIT_REF" git@github.com:ESA-Heatwise/heatwise-patch-extraction.git $PATCH_ROOT
+    git clone --branch "$GIT_REF" git@github.com:ESA-Heatwise/heatwise-lcz-classification.git $CLASSIFICATION_ROOT
+    git clone --branch "$GIT_REF" git@github.com:ESA-Heatwise/heatwise-lcz-pipeline.git $PIPELINE_ROOT
+
+    mkdir -p $INPUTS_PATH/{hsi-lst-prep,patch-extraction,lcz-classification,lcz-pipeline}
+    cp $PREP_ROOT/heatwise_hsi_lst_prep.cwl "$CWL_PATH/"
+    cp $PATCH_ROOT/heatwise_patch_extraction.cwl "$CWL_PATH/"
+    cp $CLASSIFICATION_ROOT/heatwise_lcz_predict.cwl "$CWL_PATH/"
+    cp $CLASSIFICATION_ROOT/heatwise_lcz_train.cwl "$CWL_PATH/"
+    cp $PIPELINE_ROOT/heatwise_pipeline.cwl "$CWL_PATH/"
+
+    cp -r $PREP_ROOT/examples/stac_input/* "$INPUTS_PATH/hsi-lst-prep/"
+    cp -r $PATCH_ROOT/data/Berlin "$INPUTS_PATH/patch-extraction/"
+    cp -r $CLASSIFICATION_ROOT/data/Berlin "$INPUTS_PATH/lcz-classification/"
+    cp -r $PIPELINE_ROOT/data/Berlin_prep "$INPUTS_PATH/lcz-pipeline/"
+    # cp -r $PIPELINE_ROOT/data/Berlin_labels "$INPUTS_PATH/lcz-pipeline/"
 }
 
 clean() {
@@ -41,5 +81,6 @@ if [[ $1 == "clean" ]]; then
     clean
 else
     stage_lstm_wp3_products
+    stage_hysupp
 fi
 
